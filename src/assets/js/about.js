bb60cbe98acc488e72cd6cdc9c682fa19d81e132
T0nci <138928016+T0nci@users.noreply.github.com>
2024-03-11 22:23:25 +0100

    Create and style the about page

--- src/assets/js/about.js
+++ src/assets/js/about.js
@@ -1,7 +1,30 @@
+import '../css/style.css';
+import Map from '../img/google-maps-example.jpg';
+
 function populateAbout() {
   const contentDiv = document.querySelector('#content');
 
-  contentDiv.textContent = 'about';
+  const containerDiv = document.createElement('div');
+  containerDiv.classList.add('container');
+  containerDiv.classList.add('info');
+
+  const map = new Image();
+  map.src = Map;
+  map.classList.add('map');
+
+  const location = document.createElement('p');
+  location.classList.add('location');
+  location.innerHTML = 'Location:<br>Example Street 1, Example City';
+
+  const contact = document.createElement('p');
+  contact.classList.add('contact');
+  contact.innerHTML = 'Contact:<br>+? ??? ??? ????';
+
+  containerDiv.appendChild(map);
+  containerDiv.appendChild(location);
+  containerDiv.appendChild(contact);
+
+  contentDiv.appendChild(containerDiv);
 }
 
 
